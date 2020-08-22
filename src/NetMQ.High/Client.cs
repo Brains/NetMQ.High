8c120d773838479b325181b1e3bbd99febff1fd4
Brains <brains.god@gmail.com>
2020-08-22 18:07:19 +0300

    Add TimeoutAfter method

--- src/NetMQ.High/Client.cs
+++ src/NetMQ.High/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using NetMQ.High.Engines;
 using NetMQ.High.Serializers;
@@ -62,6 +63,23 @@ namespace NetMQ.High
             return outgoingMessage.TaskCompletionSource.Task;
         }
 
+        public static async Task<TResult> TimeoutAfter<TResult>(Task<TResult> task, TimeSpan timeout)
+        {
+            using (var timeoutCancellationTokenSource = new CancellationTokenSource())
+            {
+
+                var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+                if (completedTask == task)
+                {
+                    timeoutCancellationTokenSource.Cancel();
+                    return await task;  // Very important in order to propagate exceptions
+                }
+                else
+                {
+                    throw new TimeoutException("The operation has timed out.");
+                }
+            }
+        }
         /// <summary>
         /// Send one way message to the server
         /// </summary>
