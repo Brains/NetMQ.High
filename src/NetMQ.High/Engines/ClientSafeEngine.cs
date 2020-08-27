7e8199137637d28efd9c5adc5fd7ab03bfb11f5c
Brains <brains.god@gmail.com>
2020-08-27 09:15:27 +0300

    Add ClientSafeEngine.cs

---
+++ src/NetMQ.High/Engines/ClientSafeEngine.cs
@@ -0,0 +1,10 @@
+﻿using NetMQ.High.Serializers;
+
+namespace NetMQ.High.Engines
+{
+    public class ClientSafeEngine : ClientEngine
+    {
+        public ClientSafeEngine(ISerializer serializer, NetMQQueue<OutgoingMessage> outgoingQueue, string address) : 
+            base(serializer, outgoingQueue, address) { }
+    }
+}
