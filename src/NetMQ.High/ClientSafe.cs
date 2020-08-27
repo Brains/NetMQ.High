32379037c5ac94860fee43cf1d242f1ff7a0f4b4
Brains <brains.god@gmail.com>
2020-08-27 23:06:17 +0300

    Return Task and hide base Connect

--- src/NetMQ.High/ClientSafe.cs
+++ src/NetMQ.High/ClientSafe.cs
@@ -1,15 +1,17 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using NetMQ.High.Engines;
 
 namespace NetMQ.High
 {
     public class ClientSafe : Client
     {
-        public override void Connect(string address)
+        public new Task Connect(string address)
         {
             var task = new TaskCompletionSource<object>();
             var engine = new ClientSafeEngine(serializer, m_outgoingQueue, task, address);
             m_actor = NetMQActor.Create(engine);
+            return task.Task;
         }
     }
 }
