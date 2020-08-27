e10a87250a17b583b6bdfbf2d7b20789f6d06b42
Brains <brains.god@gmail.com>
2020-08-27 06:58:46 +0300

    Move methods

--- src/NetMQ.High/Client.cs
+++ src/NetMQ.High/Client.cs
@@ -10,7 +10,7 @@ namespace NetMQ.High
     public class Client : IDisposable
     {
         private NetMQActor m_actor;
-        private NetMQQueue<ClientEngine.OutgoingMessage> m_outgoingQueue;
+        protected NetMQQueue<ClientEngine.OutgoingMessage> m_outgoingQueue;
 
         /// <summary>
         /// Create new client
@@ -46,19 +46,6 @@ namespace NetMQ.High
         }
 
         // Answer by Lawrence Johnston at https://stackoverflow.com/questions/4238345/asynchronously-wait-for-taskt-to-complete-with-timeout
-        static async Task<TResult> TimeoutAfter<TResult>(Task<TResult> task, TimeSpan timeout)
-        {
-            using (var cancellation = new CancellationTokenSource())
-            {
-                var delay = Task.Delay(timeout, cancellation.Token);
-                var completed = await Task.WhenAny(task, delay);
-
-                if (completed != task) throw new TimeoutException("The operation has timed out.");
-
-                cancellation.Cancel();
-                return await task;  // Very important in order to propagate exceptions
-            }
-        }
 
         public Task<byte[]> SendRequestAsync(string service, byte[] message)
         {
@@ -67,16 +54,6 @@ namespace NetMQ.High
             // NetMQQueue is thread safe, so no need to lock
             m_outgoingQueue.Enqueue(outgoingMessage);
             return outgoingMessage.TaskCompletionSource.Task;
-        }
-
-        public Task<byte[]> SendRequestAsyncWithTimeout(string service, byte[] message, int timeout)
-        {
-            var outgoing = new ClientEngine.OutgoingMessage(new TaskCompletionSource<byte[]>(), service, message, false);
-            // NetMQQueue is thread safe, so no need to lock
-            m_outgoingQueue.Enqueue(outgoing);
-            return TimeoutAfter(
-                outgoing.TaskCompletionSource.Task,
-                TimeSpan.FromMilliseconds(timeout));
         }
 
         /// <summary>
--- src/NetMQ.High/TimeoutClient.cs
+++ src/NetMQ.High/TimeoutClient.cs
@@ -1,7 +1,36 @@
-﻿namespace NetMQ.High
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NetMQ.High.Engines;
+
+namespace NetMQ.High
 {
     public class TimeoutClient : Client
     {
         public TimeoutClient(string address) : base(address) { }
+
+        static async Task<TResult> TimeoutAfter<TResult>(Task<TResult> task, TimeSpan timeout)
+        {
+            using (var cancellation = new CancellationTokenSource())
+            {
+                var delay = Task.Delay(timeout, cancellation.Token);
+                var completed = await Task.WhenAny(task, delay);
+
+                if (completed != task) throw new TimeoutException("The operation has timed out.");
+
+                cancellation.Cancel();
+                return await task;  // Very important in order to propagate exceptions
+            }
+        }
+
+        public Task<byte[]> SendRequestAsyncWithTimeout(string service, byte[] message, int timeout)
+        {
+            var outgoing = new ClientEngine.OutgoingMessage(new TaskCompletionSource<byte[]>(), service, message, false);
+            // NetMQQueue is thread safe, so no need to lock
+            m_outgoingQueue.Enqueue(outgoing);
+            return TimeoutAfter(
+                outgoing.TaskCompletionSource.Task,
+                TimeSpan.FromMilliseconds(timeout));
+        }
     }
 }
