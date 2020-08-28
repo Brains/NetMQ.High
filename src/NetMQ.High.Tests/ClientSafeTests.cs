fae2193310fb943fca395e7c61835951daf5e5d0
Brains <brains.god@gmail.com>
2020-08-28 23:02:51 +0300

    Init_WithNullAddress_NotFails

---
+++ src/NetMQ.High.Tests/ClientSafeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NetMQ.High.Tests
+{
+    [TestFixture]
+    class ClientSafeTests
+    {
+        [Test]
+        public void Init_WithNullAddress_NotFails()
+        {
+            using (var client = new ClientSafe(null))
+            Assert.DoesNotThrow(() => client.Init());
+        }
+    }
+}
