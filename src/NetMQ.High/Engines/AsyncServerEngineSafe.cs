ee81052d39db19f1571b91625fcff6d78c627b58
Brains <brains.god@gmail.com>
2020-08-28 18:48:57 +0300

    OnShimCommand

--- src/NetMQ.High/Engines/AsyncServerEngineSafe.cs
+++ src/NetMQ.High/Engines/AsyncServerEngineSafe.cs
@@ -6,18 +6,17 @@ namespace NetMQ.High.Engines
 {
     public class AsyncServerEngineSafe : AsyncServerEngine
     {
-        public AsyncServerEngineSafe(ISerializer serializer, IAsyncHandler asyncHandler) 
-            : base(serializer, asyncHandler) { }
+        public AsyncServerEngineSafe(ISerializer serializer, IAsyncHandler asyncHandler)
+            : base(serializer, asyncHandler) => 
+                Source = new TaskCompletionSource<object>();
 
         public TaskCompletionSource<object> Source { get; set; }
 
-        protected override void Initialize()
+        protected override void OnShimCommand(string command)
         {
-            Source = new TaskCompletionSource<object>();
-
             try
             {
-                base.Initialize();
+                base.OnShimCommand(command);
                 Source.SetResult(null);
             }
             catch (Exception e)
