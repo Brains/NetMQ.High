53a4dcb8fce3aa9685d215fc62345af14fb47a5d
Brains <brains.god@gmail.com>
2020-08-29 17:47:02 +0300

    Add draft tests

---
+++ src/NetMQ.High.Tests/ClientTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NetMQ.High.Tests
+{
+    public class ClientTests
+    {
+        class Handler : IAsyncHandler
+        {
+            public void HandleOneWay(ulong messageId, uint connectionId, string service, byte[] body) { }
+            public Task<byte[]> HandleRequestAsync(ulong messageId, uint connectionId, string service, byte[] body) =>
+                Task.FromResult(new byte[] { });
+        }
+
+        [Test]
+        public void Dispose_NotInitialized_ThrowsFaultException()
+        {
+            //using (var server = new AsyncServer(new Handler()))
+            {
+                //server.Bind("inproc://test");
+                var client = new ClientSafe("inproc://test");
+                //client.Init();
+                //client.Task.Wait();
+                client.Dispose();
+                Assert.Throws<ArgumentNullException>(() => client.Dispose());
+            }
+        }
+
+        [Test]
+        public void Dispose_Twice_ThrowsFaultException()
+        {
+            //using (var server = new AsyncServer(new Handler()))
+            {
+                //server.Bind("inproc://test");
+                var client = new ClientSafe("inproc://test");
+                //client.Init();
+                //client.Task.Wait();
+                client.Dispose();
+                Assert.Throws<FaultException>(() => client.Dispose(), "Cannot close an uninitialised Msg.");
+            }
+        }
+
+        [Test]
+        public void Dispose2_Twice_ThrowsFaultException()
+        {
+            //using (var server = new AsyncServer(new Handler()))
+            {
+                //server.Bind("inproc://test");
+                var client = new ClientSafe("inproc://test");
+                client.Init();
+                //client.Task.Wait();
+                client.Dispose();
+                Assert.Throws<FaultException>(() => client.Dispose(), "Cannot close an uninitialised Msg.");
+            }
+        }
+
+        [Test]
+        public void SendRequestAsync_DisposedClient_()
+        {
+            var client = new ClientSafe("inproc://test");
+            client.Init();
+            // client.Task.Wait();
+            client.Dispose();
+            Task.Delay(2000).Wait();
+            var message = Encoding.ASCII.GetBytes("World");
+            //client.SendRequestAsync("serice", message);
+            client.Dispose();
+        }
+    }
+}
