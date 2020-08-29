775c2ab355a889c24a46cef4fe3474f19f154a08
Brains <brains.god@gmail.com>
2020-08-29 18:09:22 +0300

    Cleanup

--- src/NetMQ.High.Tests/ClientSafeTests2.cs
+++ src/NetMQ.High.Tests/ClientSafeTests2.cs
@@ -18,13 +18,12 @@ namespace NetMQ.High.Tests
         [Ignore]
         public void Template()
         {
-            //using (var server = new AsyncServer(new Handler()))
+            using (var server = new AsyncServer(new Handler()))
             {
-                //server.Bind("inproc://test");
+                server.Bind("inproc://test");
                 var client = new ClientSafe("inproc://test");
-                //client.Init();
-                //client.Task.Wait();
-                client.Dispose();
+                client.Init();
+                client.Task.Wait();
                 Assert.Throws<ArgumentNullException>(() => client.Dispose());
             }
         }
@@ -55,33 +54,6 @@ namespace NetMQ.High.Tests
             var client = new ClientSafe("inproc://test");
             Assert.Throws<ArgumentNullException>(
                 () => client.Dispose());
-        }
-
-        [Test]
-        public void Dispose2_Twice_ThrowsFaultException()
-        {
-            //using (var server = new AsyncServer(new Handler()))
-            {
-                //server.Bind("inproc://test");
-                var client = new ClientSafe("inproc://test");
-                client.Init();
-                //client.Task.Wait();
-                client.Dispose();
-                Assert.Throws<FaultException>(() => client.Dispose(), "Cannot close an uninitialised Msg.");
-            }
-        }
-
-        [Test]
-        public void SendRequestAsync_DisposedClient_()
-        {
-            var client = new ClientSafe("inproc://test");
-            client.Init();
-            // client.Task.Wait();
-            client.Dispose();
-            Task.Delay(2000).Wait();
-            var message = Encoding.ASCII.GetBytes("World");
-            //client.SendRequestAsync("serice", message);
-            client.Dispose();
         }
     }
 }
